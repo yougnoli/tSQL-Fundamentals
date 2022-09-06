029c5829b4501255bcb263e5e0cd32c8b5b38b08
Alessio Tugnoli <77077281+yougnoli@users.noreply.github.com>
2022-09-06 13:07:21 +0200

    Update exercises.sql

--- ch02/exercises.sql
+++ ch02/exercises.sql
@@ -1,1 +1,39 @@
+-- 1
+-- Return orders placed in June 2021
+-- Tables involved: TSQLV6 database, sales.orders table
+-- Rows affected: 30
 
+-- 2
+-- Return orders placed in June 2021
+-- Tables involved: TSQLV6 database, sales.orders table
+-- Rows affected: 30
+
+-- 3
+-- Return orders placed in June 2021
+-- Tables involved: TSQLV6 database, sales.orders table
+-- Rows affected: 30
+
+-- 4
+-- Return orders placed in June 2021
+-- Tables involved: TSQLV6 database, sales.orders table
+-- Rows affected: 30
+
+-- 5
+-- Return orders placed in June 2021
+-- Tables involved: TSQLV6 database, sales.orders table
+-- Rows affected: 30
+
+-- 6
+-- Return orders placed in June 2021
+-- Tables involved: TSQLV6 database, sales.orders table
+-- Rows affected: 30
+
+-- 7
+-- Return orders placed in June 2021
+-- Tables involved: TSQLV6 database, sales.orders table
+-- Rows affected: 30
+
+-- 8
+-- Return orders placed in June 2021
+-- Tables involved: TSQLV6 database, sales.orders table
+-- Rows affected: 30
