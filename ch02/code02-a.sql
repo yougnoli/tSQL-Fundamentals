e695ff63dfb5536cbbe79e45049e3b30f2de585f
Alessio Tugnoli <77077281+yougnoli@users.noreply.github.com>
2022-09-08 10:35:59 +0200

    Update code02-a.sql

--- ch02/code02-a.sql
+++ ch02/code02-a.sql
@@ -1,4 +1,3 @@
-
 -- The FROM Clause
 -- The FROM Clause
 select
@@ -59,27 +58,76 @@ group by
 ;
 go
 
-/*
-all attributes or expressions in the select clause that are not aggregate functions must go into the group by
-*/
--- this will give me an error (freight: no calculation on him, not in the group by clause)
+-- count(*), count(attribute) and count(DISTINCT attribute)
+-- first of all I create a temp table with some values in it:
+create table #temp_table (
+	attribute int
+)
+;
+go
+-- inserting some values in it
+insert into #temp_table values
+(30)
+,(10)
+,(null)
+,(10)
+,(10)
+;
+go
+-- 5 rows affected:
+select * from #temp_table
+;
+go
+
+-- count(*) -> all rows in the column
+select
+	count(*) as num_rows
+from 
+	#temp_table
+;
+go
+
+-- count(attribute) -> all not null rows in the column
+select 
+	count(attribute) num_not_null_rows
+from
+	#temp_table
+;
+go
+
+-- count( DISTINCT attribute) -> count of distinct values in the column
+select 
+	count( distinct attribute) num_of_distinct_values
+from
+	#temp_table
+;
+go
+
+-- DISTINCT can be used with other functions:
+select
+	sum(attribute) sum_values
+from 
+	#temp_table
+;
+go
+
+-- DISTINCT can be used with other functions:
+select
+	sum(distinct attribute) sum_distinct_30_10
+from 
+	#temp_table
+;
+go
+
+-- the number of distinct (different) customers handled by each employee in each order year:
 select
 	empid
 	,year(orderdate) as order_year
-	,freight
+	,count(distinct custid) as num_customers
 from
 	sales.orders
-where
-	custid = 71
-group by 
+group by
 	empid
 	,year(orderdate)
 ;
 go
-
-
-
-
-
-
-
